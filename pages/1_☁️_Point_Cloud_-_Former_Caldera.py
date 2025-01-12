d9efd440adbe7a8ab6272fa480a0416fb7c5cc62
Garrett Speed <garrett@garrettspeed.com>
2025-01-12 14:38:47 +0000

    Update point clouds with index.html

--- pages/1_☁️_Point_Cloud_-_Former_Caldera.py
+++ pages/1_☁️_Point_Cloud_-_Former_Caldera.py
@@ -3,5 +3,5 @@ import streamlit.components.v1 as components
 
 st.write("# Former Caldera Point Cloud")
 
-components.iframe("https://vesuvius-panoramas.s3.eu-central-1.amazonaws.com/OldCaldera/", 
+components.iframe("https://vesuvius-panoramas.s3.eu-central-1.amazonaws.com/OldCaldera/index.html", 
                   height=800)
