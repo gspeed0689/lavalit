23594a18b093db8b854b205f75bb985d69318d60
Garrett Speed <garrett@garrettspeed.com>
2025-01-14 20:54:33 +0000

    Add sketchfab model

---
+++ pages/5_✏️_Sketchfab_Model.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.write("# Small Mesh Section on Sketchfab")
+
+components.iframe("https://sketchfab.com/models/da8c485f948a4cb18e31986c7154fda0/embed", 
+                  height=800)
